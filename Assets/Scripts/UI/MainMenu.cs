816e843f2db6b4f60278e4a5104602b0dae20308
Adrien Peyromaure-Debord-Broca <adrien@unity3d.com>
2020-02-01 07:24:42 -0500

    [WIP] Add MainMenu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MenuPanel;
+
+    public GameObject CreditsPanel;
+
+    public void ClickPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ClickCredits()
+    {
+        CreditsPanel.SetActive(true);
+
+        MenuPanel.SetActive(false);
+    }
+
+    public void ToggleSound()
+    {
+        //TBD
+    }
+}
