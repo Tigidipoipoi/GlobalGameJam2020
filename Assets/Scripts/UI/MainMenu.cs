ac4d96844b448f9e5b58a0749158f3dc20642f3a
Adrien Peyromaure-Debord-Broca <adrien@unity3d.com>
2020-02-01 16:12:00 -0500

    [WIP] Integrate sfx/music mute

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject MenuPanel;
 
     public GameObject CreditsPanel;
+
+    public Image MusicIcon;
+
+    public Sprite MuteMusicSprite;
+
+    public Sprite UnmuteMusicSprite;
+
+    bool m_IsMusicMute;
+
+    public Image SfxIcon;
+
+    public Sprite MuteSfxSprite;
+
+    public Sprite UnmuteSfxSprite;
+
+    bool m_IsSfxMute;
+
+    void Start()
+    {
+        //Force music on at start.
+        m_IsMusicMute = true;
+        ToggleMusic();
+
+        //Force sfx on at start.
+        m_IsSfxMute = true;
+        ToggleSfx();
+    }
 
     public void ClickPlay()
     {
@@ -19,8 +48,25 @@ public class MainMenu : MonoBehaviour
         MenuPanel.SetActive(false);
     }
 
-    public void ToggleSound()
+    public void ToggleMusic()
     {
-        //TBD
+        string eventName = m_IsMusicMute ? "Reset_Music_Bus_Vol" : "Set_Music_Bus_Vol_to_0";
+
+        AkSoundEngine.PostEvent(eventName, gameObject);
+
+        m_IsMusicMute = !m_IsMusicMute;
+
+        MusicIcon.sprite = m_IsMusicMute ? MuteMusicSprite : UnmuteMusicSprite;
+    }
+
+    public void ToggleSfx()
+    {
+        string eventName = m_IsSfxMute ? "Reset_SFX_Bus_Vol" : "Set_SFX_Bus_Vol_to_0";
+
+        AkSoundEngine.PostEvent(eventName, gameObject);
+
+        m_IsSfxMute = !m_IsSfxMute;
+
+        SfxIcon.sprite = m_IsSfxMute ? MuteSfxSprite : UnmuteSfxSprite;
     }
 }
