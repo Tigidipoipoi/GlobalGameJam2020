d47e2514b4c0c250c6ecd22093b88b5da4bb9a45
Joelle Vitzikam <joellevitzikam@gmail.com>
2020-02-01 13:36:01 -0500

    + basic loot stuff

---
+++ Assets/Scripts/Data/Loot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Drop
+{
+    public Elements Type;
+
+    public int Amount;
+}
+
+[System.Serializable]
+public class Loot
+{
+    public List<Drop> DropList;
+}
+
