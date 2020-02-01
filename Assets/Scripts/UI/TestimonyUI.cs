0b21480d43caaaca0ec37e06aae81c79e027353b
Adrien Peyromaure-Debord-Broca <adrien@unity3d.com>
2020-02-01 14:56:05 -0500

    [WIP] Testimony screen

---
+++ Assets/Scripts/UI/TestimonyUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TestimonyUI : MonoBehaviour
+{
+    public TestimonyState Data;
+
+    void Start()
+    {
+        GameFlow.StateStarted += OnStateStarted;
+        GameFlow.StateEnded += OnStateEnded;
+    }
+
+    void OnDestroy()
+    {
+        GameFlow.StateEnded -= OnStateEnded;
+        GameFlow.StateStarted -= OnStateStarted;
+    }
+
+    void OnStateStarted(GameState state)
+    {
+        if (state == Data)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    void OnStateEnded(GameState state)
+    {
+        if (state == Data)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
