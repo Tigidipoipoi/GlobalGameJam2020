7e6df1392647dbd05c3c997035c9b41a052d5628
Joelle Vitzikam <joellevitzikam@gmail.com>
2020-02-02 16:24:21 -0500

    victory

--- Assets/Scripts/States/VictoryState.cs
+++ Assets/Scripts/States/VictoryState.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Text;
 
 [CreateAssetMenu(menuName = "GameState/Victory")]
 public class VictoryState : GameState
 {
+    public TestimonyHeroLine VictoryLine;
+    public TestimonyLine StoryLine;
+
+    StringBuilder m_TestimonyBuilder = new StringBuilder();
     public override void Apply() { }
 
     public override void Enter()
@@ -16,5 +21,19 @@ public class VictoryState : GameState
     public override void Exit()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public string GetVictory() => m_TestimonyBuilder.ToString();
+
+    public void VictoryTestify()
+    {
+        if (VictoryLine != null)
+        {
+            m_TestimonyBuilder.AppendLine(VictoryLine.Text);
+        }
+        if (StoryLine != null)
+        {
+            m_TestimonyBuilder.AppendLine(StoryLine.Text);
+        }
     }
 }
---
+++ Assets/Scripts/UI/VictoryUI.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class VictoryUI : MonoBehaviour
+{
+    public VictoryState Data;
+
+    public TextMeshProUGUI Text;
+
+    void Start()
+    {
+        GameFlow.StateStarted += OnStateStarted;
+        GameFlow.StateEnded += OnStateEnded;
+
+        var gameFlow = FindObjectOfType<GameFlow>();
+        if (gameFlow != null
+            && gameFlow.CurrentState != Data)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnDestroy()
+    {
+        GameFlow.StateEnded -= OnStateEnded;
+        GameFlow.StateStarted -= OnStateStarted;
+    }
+
+    void OnStateStarted(GameState state)
+    {
+        if (state == Data)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    void OnStateEnded(GameState state)
+    {
+        if (state == Data)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnEnable()
+    {
+        Text.text = Data.GetVictory();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Data.IsDone = true;
+            AkSoundEngine.PostEvent("Play_click", gameObject);
+        }
+    }
+}
